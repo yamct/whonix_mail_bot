d344e8b1ff56c287f77fe4444c647c6cb11e0ba1
yamct <yamct@rambler.ru>
2013-07-07 00:57:54 +0000

    EmailCreds class is now named Account, holds useful info like creds, ports etc.

--- mail_bot.py
+++ mail_bot.py
@@ -1,26 +1,47 @@
 import smtplib
 import poplib
 
-def send_email(fromaddr, toaddr, username, password, content, server, port, verbosity=1):
+
+class Account:
+    """Stores necessary info for sending and receiving email"""
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    def sending_info(self, server, port=25, encryption = 'SSL'):
+        self.smtp_server = server    
+        self.smtp_port = port
+        self.smtp_encryption = encryption
+
+    def receiving_info(self, protocol, server, port, encryption = 'SSL'):
+        self.receiving_server = server
+        self.receiving_port = port
+        self.receiving_encryption = encryption
+        self.receiving_protocol = protocol                
+
+def send_email(account, fromaddr, toaddr, content, verbosity=1):
     msg = ("From: %s\r\nTo: %s\r\n\r\n"
        % (fromaddr, ", ".join(toaddr.split())))
     msg += content
-    server = smtplib.SMTP_SSL(server,port)
+    server = smtplib.SMTP_SSL(account.smtp_server, account.smtp_port)
     server.set_debuglevel(verbosity)
-    server.login(username, password)
+    server.login(account.username, account.password)
     server.sendmail(fromaddr, toaddr, msg)
     server.quit()
     return 'Email Sent'
 
-def receive_emails(username, password, server, port, protocol='POP', encryption='SSL',verbosity=1):
-    M = poplib.POP3_SSL(server, port)
+def receive_email(account, verbosity=1):
+    M = poplib.POP3_SSL(account.receiving_server, account.receiving_port)
     M.set_debuglevel(verbosity)
-    M.user(username)
-    M.pass_(password)
+    M.user(account.username)
+    M.pass_(account.password)
     num_messages = len(M.list()[1])
     messages = [M.retr(i+1) for i in xrange(0, num_messages)]
-    print messages          
+    M.quit()
+    if verbosity == 1:
+        print messages
     return messages
 
-def delete_emails(username, password, server, port, protocol='POP', encryption='SSL',verbosity=1):
-
+#def delete_emails(account, verbosity=1):
+    
